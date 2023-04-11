c0d2c7122d181db74a3174ce8c3b4a702ebd0e7f
denyanwardev <denyanwardev@gmail.com>
2023-04-11 15:02:38 +0800

    detail page done

--- lib/views/detailsview.dart
+++ lib/views/detailsview.dart
@@ -12,6 +12,8 @@ class DetailsView extends StatefulWidget {
 }
 
 class _DetailViewState extends State<DetailsView> {
+  bool _isZoomed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +21,8 @@ class _DetailViewState extends State<DetailsView> {
         child: Stack(
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 32.0),
+              padding:
+                  const EdgeInsets.only(left: 20.0, right: 20.0, top: 32.0),
               child: SingleChildScrollView(
                 physics: const BouncingScrollPhysics(),
                 child: Column(
@@ -38,9 +41,130 @@ class _DetailViewState extends State<DetailsView> {
                       ),
                       textAlign: TextAlign.left,
                     ),
+                    Text(
+                      'Solar System',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: 'Roboto-Regular',
+                        color: primaryTextColor,
+                        fontWeight: FontWeight.w300,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    const Divider(
+                      color: Colors.black38,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      height: 140,
+                      width: MediaQuery.of(context).size.width * 0.95,
+                      child: SingleChildScrollView(
+                        physics: const BouncingScrollPhysics(),
+                        scrollDirection: Axis.vertical,
+                        child: Text(
+                          widget.planetInfo!.description.toString(),
+                          style: TextStyle(
+                            fontSize: 15,
+                            overflow: TextOverflow.ellipsis,
+                            fontFamily: 'Roboto-Regular',
+                            color: contentTextColor,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          textAlign: TextAlign.left,
+                          maxLines: 100,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Divider(
+                      color: Colors.black38,
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'Gallery',
+                      style: TextStyle(
+                        fontSize: 24,
+                        overflow: TextOverflow.ellipsis,
+                        fontFamily: 'Roboto-Regular',
+                        color: contentTextColor,
+                        fontWeight: FontWeight.w300,
+                      ),
+                      textAlign: TextAlign.center,
+                      maxLines: 40,
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      height: 230,
+                      child: ListView.builder(
+                        physics: const BouncingScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: widget.planetInfo!.images!.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            clipBehavior: Clip.antiAlias,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _isZoomed = !_isZoomed;
+                                });
+                              },
+                              child: AnimatedContainer(
+                                duration: Duration(milliseconds: 200),
+                                width: _isZoomed ? 400 : 230,
+                                child: AspectRatio(
+                                  aspectRatio: 1,
+                                  child: Image.network(
+                                    widget.planetInfo!.images![index],
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
                   ],
                 ),
               ),
+            ),
+            Positioned(
+              right: -70,
+              child: Hero(
+                tag: widget.planetInfo!.position,
+                child: Image.asset(
+                  widget.planetInfo!.iconImage.toString(),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 40,
+              left: 20,
+              child: Text(
+                widget.planetInfo!.position.toString(),
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 247,
+                  color: Colors.grey.withOpacity(0.2),
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back_ios_new),
             ),
           ],
         ),
